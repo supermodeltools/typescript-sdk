96ba638e1bbfdd43087c1d3eb63a67bc64d0687c
Grey Newell <greyshipscode@gmail.com>
2025-12-23 23:48:43 +0000

    Update TypeScript SDK for release v0.3.7

---
+++ src/models/index.ts
@@ -0,0 +1,22 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ClassificationStats';
+export * from './CodeGraphEnvelope';
+export * from './CodeGraphEnvelopeGraph';
+export * from './CodeGraphNode';
+export * from './CodeGraphRelationship';
+export * from './CodeGraphStats';
+export * from './DomainClassAssignment';
+export * from './DomainClassificationResponse';
+export * from './DomainFileAssignment';
+export * from './DomainFunctionAssignment';
+export * from './DomainRelationship';
+export * from './DomainSummary';
+export * from './ErrorDetailsInner';
+export * from './FunctionDescription';
+export * from './ModelError';
+export * from './SubdomainSummary';
+export * from './SupermodelArtifact';
+export * from './SupermodelIR';
+export * from './SupermodelIRGraph';
+export * from './UnassignedFunction';
